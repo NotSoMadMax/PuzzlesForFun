aaf333ce684aed5f0bbc80ab87ac9951d0f9c7d7
swang656 <swang656@gatech.edu>
2018-05-08 18:47:19 -0400

    search rotated sorted array ii

--- src/medium/SearchinRotatedSortedArray.java
+++ src/medium/SearchinRotatedSortedArray.java
@@ -32,6 +32,42 @@ public class SearchinRotatedSortedArray {
    		
     }
     
+   /*// time limit exceed
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length < 1)
+        	return -1;
+        
+        int len = nums.length;
+        if (len == 1) {
+        	if (nums[0] == target)
+        		return 0;
+        	else
+        		return -1;
+        }
+        
+        int offset = 1;
+        while(offset < len && nums[offset] >= nums[offset - 1])
+        	offset++;
+        
+        int l = offset, r = offset - 1;
+               
+        while ((l - offset + len)%len <= (r - offset + len)%len)
+        {
+            int m = ((l - offset + len)%len + ((r - offset + len)%len-(l - offset + len)%len)/2 + offset)%len;
+ 
+            if (nums[m] == target)
+                return m;
+ 
+            if (nums[m] < target)
+                l = (m + 1)%len;
+ 
+            else
+                r = (m - 1 + len)%len;
+        }
+        
+        return -1;
+    }
+    */
     public static void main(String args[]){
     		int[] nums = {1,3};
     		System.out.println(search(nums, 0));
--- src/medium/SearchinRotatedSortedArrayII.java
+++ src/medium/SearchinRotatedSortedArrayII.java
@@ -1,7 +1,39 @@
 package medium;
 
 public class SearchinRotatedSortedArrayII {
+	
+	
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length < 1)
+        	return false;
+        
+        int l = 0, r = nums.length - 1;
+        
+        while(l <= r) {
+        	int m = l + (r - l)/2;
+        	if(nums[m] == target)
+        		return true;
+        	
+        	if(nums[m] < nums[r]) { // the second half is sorted
+        		if(target > nums[m] && target <= nums[r])
+        			l = m + 1;
+        		else
+        			r = m - 1;
+        	} else if(nums[m] > nums[r]) { // the first half is sorted
+        		if(target >= nums[l] && target < nums[m])
+        			r = m - 1;
+        		else
+        			l = m + 1;
+        	} else {
+        		r--;
+        	}
+        }
+        
+        return false;
+    }	
+	
 
+    
 	public static void main(String[] args) {
 		
 
