b22e191d514037c9114eae81f907f2c7513f5b10
swang656 <swang656@gatech.edu>
2018-05-08 14:14:21 -0400

    partition list

--- src/medium/PartitionList.java
+++ src/medium/PartitionList.java
@@ -1,30 +1,42 @@
 package medium;
+import medium.RemoveNthNodeFromEndofList.ListNode;
 
 
 public class PartitionList {	
-	
-	public class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) { val = x; }
-	}	
-	
-    public static void ListNode partition(ListNode head, int x) {
+		
+    public static ListNode partition(ListNode head, int x) {
         if (head == null || head.next == null)
         	return head;
         
-        ListNode first_larger = head;
+        ListNode helper = new ListNode(0);
+        helper.next = head;
+        ListNode cur = helper, holder = helper;
         
-        while(first_larger.val <= x)
-        	first_larger = first_larger.next;
+        while(cur.next != null) {
+        	if (cur.next.val < x) {
+        		if(cur != holder) {
+        			ListNode temp = cur.next.next;
+        			cur.next.next = holder.next;
+        			holder.next = cur.next;
+        			cur.next = temp;
+        		} else {
+        			cur = cur.next;
+        		}
+        		holder = holder.next;
+        	} else {
+        		cur = cur.next;
+        	}
+        }
         
-        
+                
+        return helper.next;
     }
     
     
 	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
 		
-
 	}
 
 }
