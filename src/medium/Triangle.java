4918701ea53b200a365c2333e1d0907de6eeb5ae
swang656 <swang656@gatech.edu>
2018-08-14 10:05:04 +0800

    Triangle

---
+++ src/medium/Triangle.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Triangle {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0)
+        	return 0;
+        
+        int len = triangle.size();
+        int[][] results = new int[len][len];
+        for (int i = 0; i < len; i++){
+        	results[len-1][i] = triangle.get(len-1).get(i);
+        }
+        
+        for (int i = len - 2; i >= 0; i--){
+        	for (int j = 0; j < i + 1; j++){
+        		results[i][j] = triangle.get(i).get(j) + Math.min(results[i+1][j], results[i+1][j+1]);
+        	}
+        }
+        
+        return results[0][0];
+    }
+    
+    public static void main(String[] args){
+    	List<List<Integer>> test = new LinkedList<>();
+    	
+    }
+}
+
+
+
+
