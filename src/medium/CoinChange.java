51babb7444e44a310c94d1528130ae070c3078ed
sxw0527 <swang656@gatech.edu>
2018-10-14 16:38:38 -0400

    coin change

--- src/medium/CoinChange.java
+++ src/medium/CoinChange.java
@@ -1,6 +1,32 @@
 package medium;
 
 public class CoinChange {
+
+    // dp
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length < 1 || amount < 1)
+            return 0;
+
+        int[] dp = new int[amount + 1]; // number of coins needed to get i dollars
+        dp[0] = 0;
+        for(int i = 1; i < amount + 1; i++){
+            dp[i] = amount + 1;
+        }
+
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.length; j++){      // for each kind of coins
+                if(i - coins[j] >= 0)
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+
+        return dp[amount]==amount+1?-1:dp[amount];
+    }
+
+
+
+    // slower
+    /*
     private static int coinChange(int[] coins, int amount) {
     		if (coins.length < 1 || coins == null || amount < 0)	return -1;
         
@@ -20,6 +46,6 @@ public class CoinChange {
         }
         
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];       	
-    }
+    }*/
 
 }
