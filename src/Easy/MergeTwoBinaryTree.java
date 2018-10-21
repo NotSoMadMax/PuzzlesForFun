e555413f5e3d247f7bbd3dc4aedcd3629707b3c7
sxw0527 <swang656@gatech.edu>
2018-10-21 14:46:35 -0400

    Merge Two Binary Tree

---
+++ src/Easy/MergeTwoBinaryTree.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class MergeTwoBinaryTree {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null)
+            return t2;
+        if(t2 == null)
+            return t1;
+
+        TreeNode head = new TreeNode(t1.val + t2.val);
+        head.left = mergeTrees(t1.left, t2.left);
+        head.right = mergeTrees(t1.right, t2.right);
+
+        return head;
+    }
+
+}
