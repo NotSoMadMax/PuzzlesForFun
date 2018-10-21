4c6238f2bc7eb830845077b00e318cceca6bc263
sxw0527 <swang656@gatech.edu>
2018-10-21 14:10:45 -0400

    Invert Binary Tree

--- src/Easy/InvertBinaryTree.java
+++ src/Easy/InvertBinaryTree.java
@@ -1,4 +1,19 @@
 package Easy;
 
 public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        if(root.left != null)
+            invertTree(root.left);
+        if(root.right != null)
+            invertTree(root.right);
+
+        return root;
+    }
 }
