14acd04b9989d07bdf51b50a911df1bfe21ea420
swang656 <swang656@gatech.edu>
2018-10-21 22:14:55 -0400

    ZigZag Conversion

--- src/medium/ZigZagConversion.java
+++ src/medium/ZigZagConversion.java
@@ -2,7 +2,26 @@ package medium;
 
 public class ZigZagConversion {
     public String convert(String s, int numRows) {
+        if(s == null || s.length() < 1 || numRows < 2)
+        	return s;
         
-    	return "";
+        StringBuilder sb = new StringBuilder();
+        char[] sa = s.toCharArray();
+        int gap = 2*numRows - 2;
+        
+        for(int i = 0; i < numRows; i++){
+        	for(int j = i; j < s.length(); j += gap){
+        		sb.append(sa[j]);
+        		
+        		if(i != 0 && i != numRows - 1){	// if not first or last line, there's elements in between periods
+        			int pos = j + 2*(numRows - i - 1);
+        			if(pos < s.length())
+        				sb.append(sa[pos]);
+        		}
+        		
+        	}
+        }
+        
+    	return sb.toString();
     }
 }
