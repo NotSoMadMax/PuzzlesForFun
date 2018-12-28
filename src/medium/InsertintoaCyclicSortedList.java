93d743a304c57d264bc3529434812b1b5e544cb0
swang656 <swang656@gatech.edu>
2018-12-28 14:42:33 -0500

    Insert into a Cyclic Sorted List

---
+++ src/medium/InsertintoaCyclicSortedList.java
@@ -0,0 +1,51 @@
+package medium;
+
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+
+class InsertintoaCyclicSortedList {
+    private Node insert(Node head, int insertVal) {
+        if(head == null){
+        	head = new Node(insertVal, null);        	
+        	head.next = head;
+        	return head;
+        }
+        
+        Node prev = head;
+        Node cur = prev.next;        
+        
+        while(cur != head){
+        	if(prev.val <= insertVal && insertVal <= cur.val)
+        		break;
+        	if((prev.val > cur.val) && (insertVal >= prev.val || insertVal <= cur.val))
+        		break;
+        	
+        	cur = cur.next;
+        	prev = prev.next;
+        }
+        
+        Node temp = new Node(insertVal, cur);
+        prev.next = temp;
+        
+        return head;
+    }
+}
+
+
+
+
+
+
+
+
+
+
