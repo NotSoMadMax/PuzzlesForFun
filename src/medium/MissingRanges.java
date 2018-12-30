13a33e254938d4d5f520e2f10c3c1fda8780131b
swang656 <swang656@gatech.edu>
2018-12-29 20:28:57 -0500

    Missing Ranges

---
+++ src/medium/MissingRanges.java
@@ -0,0 +1,38 @@
+package medium;
+
+import java.util.*;
+
+class MissingRanges {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {    
+        int len = nums.length;
+        List<String> res = new ArrayList<>();
+        long expect = lower, cur = lower;
+        for(int i = 0; i <= len; i++)
+        {        	
+        	if(i < len && nums[i] <= upper)
+        		cur = nums[i];
+        	else
+        		cur = (long)upper + 1;
+        	
+        	if(expect == cur)
+        		expect++;        	
+        	else if(expect < cur){
+        		if(expect + 1 == cur)
+        			res.add(String.valueOf(expect));
+        		else
+        			res.add(expect + "->" + (cur - 1));
+        		
+        		expect = cur + 1;
+        	}
+        }
+
+        return res;
+    }
+}
+
+
+
+
+
+
+
