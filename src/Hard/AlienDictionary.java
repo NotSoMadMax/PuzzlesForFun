a7357f3758afa3c41f110870a64ba540851c5ba3
swang656 <swang656@gatech.edu>
2019-01-25 21:43:20 -0500

    Alien Dictionary(fix bug)

--- src/Hard/AlienDictionary.java
+++ src/Hard/AlienDictionary.java
@@ -11,52 +11,49 @@ class AlienDictionary {
         Set<Character> exist = new HashSet<>();
         int[] in_degree = new int[26];
         
-        String prev = words[0];
-        for(int i = 1; i < words.length; i++){
-        	int a = 0, b = 0;
-        	while(a < prev.length() && b < words[i].length() && prev.charAt(a) == words[i].charAt(b)){
-        		a++;
-        		b++;
-        	}        	
-        	if(a == prev.length() || b == words[i].length()){
-        		prev = words[i];
-        		continue;
-        	}
-        		        	
-        	order.putIfAbsent(prev.charAt(a), new ArrayList<>());
-        	order.get(prev.charAt(a)).add(words[i].charAt(b));
-        	
-        	exist.add(prev.charAt(a));
-        	exist.add(words[i].charAt(b));
-        	
-        	prev = words[i];
-        }                        
         
-        if(exist.isEmpty())
-        	return String.valueOf(words[0].charAt(0));
+        for(String w:words){
+        	for(char c:w.toCharArray())
+        		exist.add(c);
+        }
+                
+        for(int i = 1; i < words.length; i++)
+        {
+        	int len = Math.min(words[i-1].length(), words[i].length());
+        	for(int j = 0; j < len; j++){
+        		if(words[i-1].charAt(j) != words[i].charAt(j)){
+        			order.putIfAbsent(words[i-1].charAt(j), new ArrayList<>());
+        			order.get(words[i-1].charAt(j)).add(words[i].charAt(j));
+        			break;
+        		}
+        	}        	        	
+        }                        
         
         for(List<Character> next:order.values()){
         	for(char c:next)
         		in_degree[c - 'a']++;        	
         }
         
+        StringBuilder sb = new StringBuilder();
         Queue<Character> queue = new LinkedList<>();        
         for(char c:exist){
-        	if(in_degree[c - 'a'] == 0)
-        		queue.add(c);        		
+        	if(in_degree[c - 'a'] == 0){
+        		queue.add(c);      
+        		sb.append(c);
+        	}        			
         }
-        
-        StringBuilder sb = new StringBuilder();
+                
         while(!queue.isEmpty())
         {
-        	char cur = queue.poll();
-        	sb.append(cur);
+        	char cur = queue.poll();        	
         	List<Character> next = order.get(cur);
         	if(next != null){
             	for(char c: next){
             		in_degree[c - 'a']--;
-            		if(in_degree[c - 'a'] == 0)
+            		if(in_degree[c - 'a'] == 0){
             			queue.add(c);
+            			sb.append(c);
+            		}            			
             	}  
         	}      	        	
         }
