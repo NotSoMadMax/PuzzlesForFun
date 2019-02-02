1a7bff8d4de5e4b3be839a746a7ce62c0d2f5185
sxw0527 <swang656@gatech.edu>
2019-02-02 15:55:27 -0500

    Partition List (Method 2)

--- src/medium/PartitionList.java
+++ src/medium/PartitionList.java
@@ -3,7 +3,8 @@ import medium.RemoveNthNodeFromEndofList.ListNode;
 
 
 public class PartitionList {	
-		
+
+	// Method 1
     public static ListNode partition(ListNode head, int x) {
         if (head == null || head.next == null)
         	return head;
@@ -31,12 +32,35 @@ public class PartitionList {
                 
         return helper.next;
     }
+
     
-    
-	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		head.next = new ListNode(2);
-		
+	// Method 2
+	public static ListNode partition2(ListNode head, int x) {
+		if (head == null || head.next == null)
+			return head;
+
+		ListNode small_head = new ListNode(0);
+		ListNode large_head = new ListNode(0);
+		ListNode cur_small = small_head, cur_large = large_head;
+
+		ListNode cur = head;
+
+		while (cur != null){
+			if(cur.val < x){
+				cur_small.next = cur;
+				cur_small = cur_small.next;
+			} else {
+				cur_large.next = cur;
+				cur_large = cur_large.next;
+			}
+
+			cur = cur.next;
+		}
+
+		cur_small.next = large_head.next;
+		cur_large.next = null;	// without this line: memory limit exceeded
+
+		return small_head.next;
 	}
 
 }
