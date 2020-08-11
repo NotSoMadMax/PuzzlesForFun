66d74c4b21e84e1f89d2d96e50116ff169c81ba2
NotSoMadMax <wwwwsnnn@gmail.com>
2020-08-10 21:30:20 -0700

    Fibonacci Number

---
+++ src/Easy/FibonacciNumber.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class FibonacciNumber {
+    public int fib(int N) {
+        int[] f = new int[N + 2];
+        f[0] = 0;
+        f[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        return f[N];
+    }
+}
