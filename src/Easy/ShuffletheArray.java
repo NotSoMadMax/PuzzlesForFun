1784e86ea8133fc15187c14bfbf4d45666ea93b2
NotSoMadMax <wwwwsnnn@gmail.com>
2020-08-11 22:17:47 -0700

    Shuffle the Array

---
+++ src/Easy/ShuffletheArray.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class ShuffletheArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[n + i];
+        }
+
+        return res;
+    }
+}
