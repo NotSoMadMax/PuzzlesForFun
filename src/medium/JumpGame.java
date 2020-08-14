07ffbf6a363a9c2250396b30768c1bbe13c14e94
NotSoMadMax <wwwwsnnn@gmail.com>
2020-08-13 23:01:07 -0700

    Jump Game

--- src/medium/JumpGame.java
+++ src/medium/JumpGame.java
@@ -8,7 +8,7 @@ public class JumpGame {
         		return false;
         if (len == 1)
         		return true;
-        
+
         int next = len - 1;
         for (int cur = len - 2; cur >= 0; cur--){
         		if (cur + nums[cur] >= next)
@@ -16,28 +16,26 @@ public class JumpGame {
         }
         return next == 0;
 	}
-	
-	/*	time limit exceed
-    private static boolean canJump(int[] nums) {
-        int len = nums.length;
-        if (len < 1 || nums == null)
-        		return false;
-        if (len == 1)
-        		return true;
-        
-        boolean[] memo = new boolean[len];
-        memo[len - 1] = true;
-        
-		for (int cur = nums.length - 2; cur >= 0; cur--){
-			int step = nums[cur];
-			for (int i = 1; i <= step && (cur + i) < len; i++){
-				if (memo[cur + i]){
-					memo[cur] = true;
-					break;
-				}    				    			
-    			}    			
-		}
-		return memo[0];				
-    }	
-    */
+
+	// 2020
+    // Greedy
+    public boolean canJump2020(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return true;
+        }
+
+        int farthest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                return false;
+            }
+            if (farthest >= nums.length - 1) {
+                return true;
+            }
+            farthest = Integer.max(farthest, i + nums[i]);
+        }
+
+        return farthest >= nums.length - 1;
+    }
 }
+                 
