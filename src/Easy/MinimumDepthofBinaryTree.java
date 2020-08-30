839a74bd16034f2a9934087e3fb3ba83be222452
NotSoMadMax <wwwwsnnn@gmail.com>
2020-08-30 14:41:55 -0700

    Minimum Depth of Binary Tree

--- src/Easy/MinimumDepthofBinaryTree.java
+++ src/Easy/MinimumDepthofBinaryTree.java
@@ -1,6 +1,23 @@
 package Easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class MinimumDepthofBinaryTree {
+
+	class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+      	  this.val = val;
+      	  this.left = left;
+      	  this.right = right;
+      }
+  	}
+
 	//calculate all node, slow
 	/*
     private int minDepth(TreeNode root) {
@@ -26,13 +43,14 @@ class MinimumDepthofBinaryTree {
     	return 1 + Math.min(left_depth, right_depth);
     }
     */
-    
-    private int minDepth(TreeNode root) {
+
+	// Method 1: DFS
+    private int minDepthDFS(TreeNode root) {
     	if (root == null)
     		return 0;
     	
-    	int left_depth = minDepth(root.left);
-    	int right_depth = minDepth(root.right);
+    	int left_depth = minDepthDFS(root.left);
+    	int right_depth = minDepthDFS(root.right);
     	
     	if(left_depth == 0 || right_depth == 0){	// if one child is null, doesn't count
     		return 1 + Math.max(left_depth, right_depth);
@@ -40,4 +58,42 @@ class MinimumDepthofBinaryTree {
     	
     	return 1 + Math.min(left_depth, right_depth);
     }
+
+    // Method 2: BFS
+	// Score: 100%
+	private int minDepthBFS(TreeNode root) {
+    	if (root == null) {
+    		return 0;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<>();
+    	queue.add(root);
+
+    	int depth = 0;
+		TreeNode cur;
+    	while (!queue.isEmpty()) {
+    		int nOfNodes = queue.size(); // Number of nodes at this level
+			for (int i = 0; i < nOfNodes; i++) {
+				cur = queue.poll();
+				if (cur.left == null && cur.right == null) {	// return when reached the first leaf
+					return depth;
+				}
+				if (cur.left != null) {
+					queue.add(cur.left);
+				}
+				if (cur.right != null) {
+					queue.add(cur.right);
+				}
+			}
+			depth++;
+		}
+    	return depth;
+	}
 }
+
+
+
+
+
+
+
