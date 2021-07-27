a588e1c278a1ca4e4ccb7c5e4d4750074e8db37a
NotSoMadMax <wwwwsnnn@gmail.com>
2021-07-26 23:56:48 -0700

    Two sum

--- src/Easy/TwoSum.java
+++ src/Easy/TwoSum.java
@@ -1,6 +1,8 @@
 package Easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     private int[] twoSum(int[] nums, int target) {
@@ -23,5 +25,50 @@ public class TwoSum {
         }
 
         return res;
+    }
+
+    public int[] twoSum2021Hash(int[] nums, int target) {
+        Map<Integer, Integer> location = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            location.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            if(location.containsKey(remain) && location.get(remain) != i) {
+                return new int[]{i, location.get(remain)};
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum2021Sort(int[] nums, int target) {
+        int[] sortedNums = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortedNums);
+        int left = 0;
+        int right = sortedNums.length - 1;
+
+        while(left < right) {
+            int sum = sortedNums[left] + sortedNums[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return findIndices(sortedNums[left], sortedNums[right], nums);
+            }
+        }
+        return null;
+    }
+
+    public int[] findIndices(int num1, int num2, int[] nums) {
+        int[] response = new int[2];
+        int numFound = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == num1 || nums[i] == num2) {
+                response[numFound++] = i;
+            }
+        }
+        return response;
     }
 }
