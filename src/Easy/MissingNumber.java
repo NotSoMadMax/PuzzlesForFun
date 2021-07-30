60cac0449468a0e46515eff952a2d7b69cb53dbb
NotSoMadMax <wwwwsnnn@gmail.com>
2021-07-29 19:55:35 -0700

    Missing number

--- src/Easy/MissingNumber.java
+++ src/Easy/MissingNumber.java
@@ -1,6 +1,6 @@
 package Easy;
 
-class Solution {
+public class Solution {
     public int missingNumber(int[] nums) {
         if(nums == null || nums.length < 1) {
         	return -1;
@@ -16,5 +16,15 @@ class Solution {
         }
 
         return expectedSum;
+    }
+
+    public int missingNumber2021(int[] nums) {
+        int sum = 0;
+        int expectedSum = 0;
+        for(int i = 1; i <= nums.length; i++) {
+            expectedSum += i;
+            sum += nums[i - 1];
+        }
+        return expectedSum - sum;
     }
 }
