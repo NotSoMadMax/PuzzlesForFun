f6dafd422aa03ea5c9d5a114af0d20799a5aae6a
NotSoMadMax <wwwwsnnn@gmail.com>
2021-08-03 19:59:27 -0700

    First Bad Version

---
+++ src/Easy/FirstBadVersion.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class FirstBadVersion {
+
+    private boolean isBadVersion(int version) {
+        return version % (int) (Math.random() * 10) == 0;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left != right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
