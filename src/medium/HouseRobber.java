48b64c2bf7d20a1e6411152ff70f3565a44ada87
NotSoMadMax <wwwwsnnn@gmail.com>
2022-09-24 16:06:16 -0700

    HouseRobber DP solution

--- src/medium/HouseRobber.java
+++ src/medium/HouseRobber.java
@@ -31,4 +31,23 @@ class Solution {
         
         return maxAmount;
     }
+
+    // Method 2: DP
+    public int rob2(int[] nums) {
+        if(nums == null || nums.length < 1) {
+            return 0;
+        }
+        if (nums.length < 2) {
+            return nums[0];
+        }
+        
+        int[] maxRobTo = new int[nums.length];
+        maxRobTo[0] = nums[0];
+        maxRobTo[1] = Math.max(maxRobTo[0], nums[1]);
+        for(int i = 2; i < nums.length; i++) {
+            maxRobTo[i] = Math.max(maxRobTo[i - 1], nums[i] + maxRobTo[i - 2]);
+        }
+   
+        return maxRobTo[nums.length - 1];
+    }
 }
